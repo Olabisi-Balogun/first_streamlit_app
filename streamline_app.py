8c7ed5d1b8c6f4deb2824087f7db07516244f7b3
Olabisi Balogun <s.olabisi@yahoo.com>
2023-10-26 09:31:08 -0500

    Update streamline_app.py
    added menu

--- streamline_app.py
+++ streamline_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberrv Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
