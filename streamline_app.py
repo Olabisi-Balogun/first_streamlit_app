8d71724a0e23dec492e2d2b874b3d65473d7f483
Olabisi Balogun <s.olabisi@yahoo.com>
2023-11-07 22:21:37 -0600

    Update streamline_app.py
    call fruity_vice API

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.title('My mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberrv Oatmeal')
@@ -18,3 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+#New Section to display fruityvice api response
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
