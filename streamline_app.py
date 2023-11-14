3c007016943847104723776b109b139f7984d5a4
Olabisi Balogun <s.olabisi@yahoo.com>
2023-11-13 22:22:36 -0600

    Update streamline_app.py
    add 'from streamlit'

--- streamline_app.py
+++ streamline_app.py
@@ -43,4 +43,6 @@ streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
 #add second user input textbox
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ',add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.dataframe(my_data_row)
